04b5c5c1d00f188785fc0d069cfb445e156abe91
SeungMinK <ksm8797@naver.com>
2022-05-18 17:46:21 +0900

    FEAT : Add Router
    - Router 기능 구현

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import Login from "./pages/sign/Login";
+import Home from "./pages/Home";
+import Border from "./pages/Home";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header"></header>
-            <section></section>
+            <section>
+                <Routes>
+                    <Route exact path="/" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/border" element={<Border />} />
+                </Routes>
+            </section>
             <footer></footer>
         </div>
     );
