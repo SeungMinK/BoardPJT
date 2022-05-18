bf62d6c64f25cb47d2ec388db23cc6eb851dbed1
SeungMinK <ksm8797@naver.com>
2022-05-18 17:08:17 +0900

    first commit

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+            <section></section>
+            <footer></footer>
+        </div>
+    );
+}
+
+export default App;
