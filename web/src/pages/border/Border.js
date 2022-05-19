bc47fec5a6907b860157a5566a535147afbd35c7
SeungMinK <ksm8797@naver.com>
2022-05-19 14:41:14 +0900

    FEAT : 화면 영역 나누기

--- web/src/pages/border/Border.js
+++ web/src/pages/border/Border.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import "../../css/border.css";
 function Border() {
     return (
         <>
-            <div>Border 페이지</div>
+            <div className="border-wrap">
+                <header className="border-wrap-top">
+                    <span>상단 영역</span>
+                </header>
+                <section className="border-wrap-middle">
+                    <span>중단 영역</span>
+                </section>
+                <footer className="border-wrap-bottom">
+                    <span>하단 영역</span>
+                </footer>
+            </div>
         </>
     );
 }
