35359fe36517984670db2a0ad16dae79c8e970e0
SeungMinKim <ksm8797@naver.com>
2022-05-22 18:21:02 +0900

    TEST : 게시판 데이터 조회

--- server/server/routers/board.js
+++ server/server/routers/board.js
@@ -6,11 +6,12 @@ const logger = require("../../public/js/logger");
 const exec_sql = require("../../public/js/exec_sql");
 require("dotenv").config();
 router.post("/inquiry", function (req, res) {
+  const selectDataQuery = `select * from tbl_board`;
   getConnection((connection) => {
     (async () => {
       try {
-        const selectDataQuery = await exec_sql(connection, selectDataQuery);
-        if (selectDataQuery) res.status(200).send(selectDataQuery);
+        const selectData = await exec_sql(connection, selectDataQuery);
+        if (selectData) res.status(200).send(selectData);
         else res.status(400).send(false);
       } catch (err) {
         console.log(err);
