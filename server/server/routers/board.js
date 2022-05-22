2a9815dc6462aac24588f87f0362a9630dde405e
SeungMinKim <ksm8797@naver.com>
2022-05-22 18:16:14 +0900

    TEST : 연결 테스트

--- server/server/routers/board.js
+++ server/server/routers/board.js
@@ -4,8 +4,20 @@ let router = express.Router();
 const getConnection = require("../../lib/db");
 const logger = require("../../public/js/logger");
 const exec_sql = require("../../public/js/exec_sql");
-
-router.get("/test", function (req, res) {
-    res.send("정상 연결");
+require("dotenv").config();
+router.post("/inquiry", function (req, res) {
+  getConnection((connection) => {
+    (async () => {
+      try {
+        const selectDataQuery = await exec_sql(connection, selectDataQuery);
+        if (selectDataQuery) res.status(200).send(selectDataQuery);
+        else res.status(400).send(false);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        connection.release();
+      }
+    })();
+  });
 });
 module.exports = router;
