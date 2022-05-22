b05fd57610553b7c8304aa62ed061271bac47649
SeungMinKim <ksm8797@naver.com>
2022-05-22 19:05:32 +0900

    FEAT : 페이지 추가

---
+++ web/src/pages/board/BoardContent.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import "../../css/board.css";
+import axios from "axios";
+
+/*게시판 컨텐츠 화면 */
+function BoardContent() {
+  const API_HOST = process.env.REACT_APP_API_HOST;
+
+  return (
+    <>
+      <h1>게시판 컨텐츠 화면</h1>
+    </>
+  );
+}
+
+export default BoardContent;
---
+++ web/src/pages/board/BoardUpdate.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import "../../css/board.css";
+import axios from "axios";
+
+/*게시판 글쓰기 화면 */
+function BoardUpdate() {
+  const API_HOST = process.env.REACT_APP_API_HOST;
+
+  return (
+    <>
+      <h1>게시판 글 수정 화면</h1>
+    </>
+  );
+}
+
+export default BoardUpdate;
